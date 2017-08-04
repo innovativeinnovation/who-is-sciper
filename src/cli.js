07dc43766b7d98e8fc9a0ad694928087c0320953
William Belle <william.belle@gmail.com>
2017-08-04 08:16:21 +0200

    display accreds

--- src/cli.js
+++ src/cli.js
@@ -42,32 +42,47 @@ var argv = yargs.argv;
 var sciper  = argv._[ 0 ];
 var locale  = 'en';
 
-var displayPerson = function(person) {
+var aggregateInfos = function(person) {
   var infos = '';
   infos += 'Name:     ' + person.firstname + ' ' + person.name + '\n';
   if (person.email) {
     infos += 'Email:    ' + person.email + '\n';
   }
-  if (person.office) {
-    infos += 'Office:   ' + person.office + '\n';
+  for (var i = 0; i < person.accreds.length; i++) {
+    infos += '\n' + 'Position: ' + person.accreds[i].position + '\n';
+    infos += 'In unit:  ' + person.accreds[i].acronym + '\n';
+    if (person.accreds[i].address) {
+      var addressPart = person.accreds[i].address.split('$');
+      infos += 'Address:  ';
+      for (var j = 0; j < addressPart.length; j++) {
+        if (j !== 0) {
+          infos += '         ';
+        }
+        infos += addressPart[j] + '\n';
+      }
+    }
+    if (person.accreds[i].office) {
+      infos += 'Office:   ' + person.accreds[i].office + '\n';
+    }
+    if (person.accreds[i].phones) {
+      infos += 'Phone:    ' + person.accreds[i].phones + '\n';
+    }
   }
-  if (person.phones) {
-    infos += 'Phone:    ' + person.phones + '\n';
-  }
-  infos += 'Unit:     ' + person.unit + '\n';
-  infos += 'Position: ' + person.position + '\n\n';
-  infos += 'See https://people.epfl.ch/cgi-bin/people?id=' + sciper;
+  return infos;
+};
 
-  var gender = detectGender.detect(person.firstname);
+var put = function(firstname, sciper, infos) {
+  infos += '\n' + 'See https://people.epfl.ch/cgi-bin/people?id=' + sciper;
+  var gender = detectGender.detect(firstname);
   if (gender === 'female') {
     try {
-      console.log(chalk.hex('#FF69B4')(infos));
+      console.log(chalk.hex('#FD4286')(infos));
     } catch (err) {
       console.log(infos);
     }
   } else if (gender === 'male') {
     try {
-      console.log(chalk.hex('#1E90FF')(infos));
+      console.log(chalk.hex('#0484FF')(infos));
     } catch (err) {
       console.log(infos);
     }
@@ -76,12 +91,20 @@ var displayPerson = function(person) {
   }
 };
 
+var displayFull = function(person) {
+  put(
+    person.firstname,
+    person.sciper,
+    aggregateInfos(person)
+  );
+};
+
 if (argv.l && argv.l === 'fr') {
   locale = 'fr';
 }
 
 epflPeopleApi.findBySciper(sciper, locale).then(function(person) {
-  displayPerson(person);
+  displayFull(person);
 }).catch(function(err) {
   console.log(err.message);
 });
